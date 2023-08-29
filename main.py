427db72338f5d00b2d8d01a7b19bfa7fcd6e2258
noeuli <noeuli@gmail.com>
2023-08-29 21:03:07 +0900

    Refactor main.py to be implemented in ui.py and ctrl.py

--- main.py
+++ main.py
@@ -1,51 +1,16 @@
-# ch 4.4.1 main.py
+# ch 5.2.1 main.py
 import sys
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, 
-                             QMessageBox, QPlainTextEdit, QHBoxLayout)
-from PyQt5.QtGui import QIcon
+from ui import View # ui.py의 View class 추가
+from ctrl import Control    # ctrl.py의 Control class 추가
+from PyQt5.QtWidgets import QApplication
+                             
+def main(): # 프로그램 실행(Application) 관련 내용 함수화
+    calc = QApplication(sys.argv)
+    app = QApplication(sys.argv)
+    view = View()   # View 인스턴스 선언
+    Control(view=view)  # Control 인스턴스 선언
+    sys.exit(app.exec_())
 
-class Calculator(QWidget):
-
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-
-    def initUI(self):
-        self.te1 = QPlainTextEdit()
-        self.te1.setReadOnly(True)
-
-        self.btn1 = QPushButton('Message', self)
-        self.btn1.clicked.connect(self.activateMessage) # link handler
-
-        self.btn2 = QPushButton('Clear', self)
-        self.btn2.clicked.connect(self.clearMessage)
-
-        hbox = QHBoxLayout()
-        hbox.addStretch(1)
-        hbox.addWidget(self.btn1)
-        hbox.addWidget(self.btn2)
-
-        vbox = QVBoxLayout()    # create vertical layout
-        vbox.addWidget(self.te1)
-        #vbox.addWidget(self.btn1)   # add button
-        vbox.addLayout(hbox)
-        vbox.addStretch(1)  # empty space
-
-        self.setLayout(vbox)    # (empty space - button - empty space) layout
-
-        self.setWindowTitle('Calculator')
-        self.setWindowIcon(QIcon('icon.png'))
-        self.resize(256, 256)
-        self.show()
-
-    def activateMessage(self):  # button click handler
-        # QMessageBox.information(self, "information", "Button clicked!")
-        self.te1.appendPlainText("Button clicked!")
-
-    def clearMessage(self): # button 2 handler
-        self.te1.clear()
 
 if __name__=='__main__':
-    app = QApplication(sys.argv)
-    view = Calculator()
-    sys.exit(app.exec_())
+    main()
